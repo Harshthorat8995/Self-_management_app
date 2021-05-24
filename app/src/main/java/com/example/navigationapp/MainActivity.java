da19f72f2506a7878749d02499482c7948867ec4
Harsh <19606@students.mrgs.school.nz>
2021-05-25 05:59:56 +1200

    added login page and amimation linked to login page and splash screen

--- app/src/main/java/com/example/navigationapp/MainActivity.java
+++ app/src/main/java/com/example/navigationapp/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.navigationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -44,8 +47,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
-                finish();
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(image, "logo_image");
+                pairs[1] = new Pair<View, String>(logo, "logo_text");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                    startActivity(intent, options.toBundle());
+                }
             }
         }, Splash_screen);
     }
